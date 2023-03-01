47f55f6fd84ae4539cb2057da3cf76e7ca24a057
Fvtvl <fvtvlnight@gmail.com>
2023-03-01 18:44:02 +0000

    start project, add firebase

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2023-03-01T18:19:21.871Z',
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
