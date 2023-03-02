54f6d04061ddbb2664a156b1dd5fead268b71424
Fvtvl <fvtvlnight@gmail.com>
2023-03-02 19:15:02 +0000

    validation email,pass, getToken, auth

--- functions/index.js
+++ functions/index.js
@@ -36,10 +36,48 @@ app.get('/screams', (req, res) => {
     .catch((err) => console.error(err));
 });
 
-app.post('/scream', (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer ')
+  ) {
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.error('No token found');
+    return res.status(403).json({ error: 'Unauthorized' });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then((data) => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch((err) => {
+      console.error('Error while verifying token ', err);
+      return res.status(403).json(err);
+    });
+};
+//post one scream
+
+app.post('/scream', FBAuth, (req, res) => {
+  if (req.body.body.trim() === '') {
+    return res.status(400).json({ body: 'Body must not be empty' });
+  }
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString(),
   };
 
@@ -54,6 +92,18 @@ app.post('/scream', (req, res) => {
     });
 });
 
+const isEmpty = (string) => {
+  if (string.trim() === '') return true;
+  else return false;
+};
+
+const isEmail = (email) => {
+  const emailRegEx =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(emailRegEx)) return true;
+  else return false;
+};
+
 //signup route
 
 app.post('/signup', (req, res) => {
@@ -63,6 +113,27 @@ app.post('/signup', (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle,
   };
+
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty';
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address';
+  }
+
+  if (isEmpty(newUser.password)) {
+    errors.password = 'Must not be empty';
+  }
+
+  if (newUser.confirmPassword !== newUser.password)
+    errors.confirmPassword = 'Passwords must match';
+
+  if (isEmpty(newUser.handle)) {
+    errors.handle = 'Must not be empty';
+  }
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
   //TODO validate data
   let token, userId;
@@ -103,6 +174,44 @@ app.post('/signup', (req, res) => {
       } else {
         return res.status(500).json({ error: err.code });
       }
+    });
+});
+
+//log in route
+
+app.post('/login', (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  let errors = {};
+
+  if (isEmpty(user.email)) {
+    errors.email = 'Must not be empty';
+  } else if (!isEmail(user.email)) {
+    errors.email = 'Must be a valid email address';
+  }
+  if (isEmpty(user.password)) errors.password = 'Must not be empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === 'auth/wrong-password') {
+        return res
+          .status(403)
+          .json({ general: 'Wrong credentials, please try again' });
+      } else return res.status(500).json({ error: err.code });
     });
 });
 
