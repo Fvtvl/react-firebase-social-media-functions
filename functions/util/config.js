6ce1755aa30b3958edea6e6583bdd9c044c79266
Fvtvl <fvtvlnight@gmail.com>
2023-03-03 12:22:43 +0000

    refactoring index.js

---
+++ functions/util/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: 'AIzaSyDfQGxtFw76rIoem-SI3vGBIdYP3kWIIzQ',
+  authDomain: 'socialapp-c12eb.firebaseapp.com',
+  projectId: 'socialapp-c12eb',
+  storageBucket: 'socialapp-c12eb.appspot.com',
+  messagingSenderId: '1072267479083',
+  appId: '1:1072267479083:web:aeda54daa5d8f43f78026e',
+};
