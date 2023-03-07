c9a3873925b190d0faaa2bc5754bc0cf588f4193
Fvtvl <fvtvlnight@gmail.com>
2023-03-07 14:19:09 +0000

    add like, unlike, delete scream functions

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,9 @@ const {
   postOneScream,
   getScream,
   commentOnScream,
+  likeScream,
+  unlikeScream,
+  deleteScream,
 } = require('./handlers/screams');
 const {
   signup,
@@ -23,9 +26,10 @@ app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
 
-//TODO: delete scream
-//TODO: like a scream
-//TODO: unlike a scream
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
+
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 //users routes
